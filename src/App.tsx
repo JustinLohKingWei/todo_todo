eaa48725ff24ec086640b9a6276b22a588ddfe10
Justin Loh <justinlohkingwei@gmail.com>
2021-12-27 22:52:06 -0500

    Typescript Conversion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import InputBox from './components/InputBox';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header/>
       <InputBox/>
     </div>
